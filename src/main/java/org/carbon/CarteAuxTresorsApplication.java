256a843f48a424bdb5bd92136f2aff136f8f2c23
yasser-kb <elkobi.yassir.tsi@gmail.com>
2024-11-02 20:07:24 +0100

    first commit | springboot initialisation

---
+++ src/main/java/org/carbon/CarteAuxTresorsApplication.java
@@ -0,0 +1,13 @@
+package org.carbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteAuxTresorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteAuxTresorsApplication.class, args);
+	}
+
+}
